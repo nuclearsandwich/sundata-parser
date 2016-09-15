151677fdc22a59ac47c88e17f59f73e5b2f21f43
Steven! Ragnarök <steven@nuclearsandwich.com>
2016-09-14 17:32:13 -0700

    Parse tabular data and record as output.
    The parse method is getting pretty heavy, but it's also the bulk of what
    we're doing and I'm not sure it makes sense to decompose it yet.
    
    The sundata files are divided into frontmatter data and tabular data.
    The only indication that we're entering the Tabular section is the table
    header so we use that as a key to the special variable
    `arrived_at_tabular_data` a mouthful but accurate.
    
    There's a few frontmatter checks added already, but they're mostly
    skipping lines that don't contain anything worth recording.
    Later we'll add things like the date and other metadata up there.
    
    For now we're mostly focused on pulling out the tabular data from each
    table entry.

--- sundata_parser.rb
+++ sundata_parser.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require_relative "sammy"
+
 class SundataParser
   attr_reader :input_files, :input_data, :output_data
 
@@ -18,6 +20,38 @@ class SundataParser
   end
 
   def parse
+    input_data.each do |filename, data|
+      site = Sammy.site_from_filename filename
+      year = Sammy.year_from_filename filename
+      line_data = { "site" => site, "year" => year }
+
+      arrived_at_table_data = false
+
+      data.split("\r\n").each do |line|
+        next if line.strip.empty? # Skip blank and whitespace lines
+
+        if !arrived_at_table_data
+          next if line =~ /\ACreated by SunData/ # Skip created by line
+
+          # Once we hit the table hearder we can start processing tabular data.
+          arrived_at_table_data = true and next if line =~ /\ATime\tPlot\t/
+        else
+          table_line = line.split("\t")
+          line_data["time"] = table_line[0]
+          line_data["plot"] = table_line[1]
+          line_data["sample"] = table_line[2]
+          line_data["transmitted"] = table_line[3]
+          line_data["spread"] = table_line[4]
+          line_data["incident"] = table_line[5]
+          line_data["beam"] = table_line[6]
+          line_data["zenith angle"] = table_line[7]
+          line_data["lai"] = table_line[8]
+          line_data["notes"] = table_line[9]
+          # Only record output data once the full line data has been captured.
+          output_data << line_data
+        end
+      end
+    end
   end
 
   def write_csv filename
--- test/test_sundata_parser.rb
+++ test/test_sundata_parser.rb
@@ -18,6 +18,20 @@ class TestSundataParser < MiniTest::Test
     end
   end
 
+  def test_parses_sunscan_table_attributes
+    sample_data = @parser.output_data.first
+    assert_equal "13:12:37", sample_data["time"]
+    assert_equal "21", sample_data["plot"]
+    assert_equal "4", sample_data["sample"]
+    assert_equal "164.0", sample_data["transmitted"]
+    assert_equal "0.38", sample_data["spread"]
+    assert_equal "1770.8", sample_data["incident"]
+    assert_equal "0.80", sample_data["beam"]
+    assert_equal "14.3", sample_data["zenith angle"]
+    assert_equal "4.6", sample_data["lai"]
+    assert_equal nil, sample_data["notes"]
+  end
+
   def test_writes_out_csv_file
     sunscan_files = ["sam 01 _6 27.TXT", "sam 03 _6 27.TXT", "sam 04 _7_1.TXT", "sam 05 _6 28.TXT", "sam 06 6_30.TXT"].map{|name| File.join(@fixture_root, name)}
     outfile = File.join(@fixture_root, "2011-test-output.csv")
